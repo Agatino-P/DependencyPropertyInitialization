7ae14f70a838cc455b1608d6ddcebc107146220f
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-10-09 23:28:51 +0200

    Add project files.

---
+++ DependencyPropertyInitialization/ClassDPUserControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyPropertyInitialization
+{
+    public partial class ClassDPUserControl : UserControl
+    {
+
+        public NamedClass NamedClassDP
+        {
+            get { return (NamedClass)GetValue(NamedClassDPProperty); }
+            set { SetValue(NamedClassDPProperty, value); }
+        }
+        //public static readonly DependencyProperty NamedClassDPProperty =
+        //    DependencyProperty.Register("NamedClassDP", typeof(NamedClass), typeof(ClassDPUserControl), new PropertyMetadata(new NamedClass()));
+
+        public static readonly DependencyProperty NamedClassDPProperty =
+        DependencyProperty.Register("NamedClassDP", typeof(NamedClass), typeof(ClassDPUserControl)) ;
+
+        public ClassDPUserControl()
+        {
+            InitializeComponent();
+            NamedClassDP = new NamedClass();
+        }
+    }
+}
---
+++ DependencyPropertyInitialization/NamedClass.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+
+namespace DependencyPropertyInitialization
+{
+    public class NamedClass : ViewModelBase
+    {
+        private static int classCounter = 0;
+
+        private int _counter; public int Counter { get => _counter; set { Set(() => Counter, ref _counter, value); }}
+
+
+        public NamedClass()
+        {
+            Counter = classCounter++;
+        }
+
+        public override string ToString()
+        {
+            return Counter.ToString();
+        }
+    }
+}
